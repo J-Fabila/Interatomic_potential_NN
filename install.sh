6c00c3cd2c14f8e0bdfe916f07acd8757d945d60
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2021-12-12 18:59:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
-echo  -e \\e[94m "Downloading necessary files"
-
+echo  -e \\e[32m " #########################################################"
+echo  -e \\e[32m " ############################# Downloading necessary files"
+echo  -e \\e[32m " #########################################################"
+echo  -e \\e[94m " "
 wget https://raw.githubusercontent.com/J-Fabila/VASP_to_XYZ/master/atomicpp.h
-wget https://raw.githubusercontent.com/J-Fabila/Interatomic_potential_NN/main/geometrias.cpp
+wget https://raw.githubusercontent.com/J-Fabila/Interatomic_potential_NN/main/forces.cpp
 wget https://raw.githubusercontent.com/J-Fabila/Interatomic_potential_NN/main/distortions.cpp
 wget https://raw.githubusercontent.com/J-Fabila/Interatomic_potential_NN/main/sampling.py
-wget https://raw.githubusercontent.com/J-Fabila/Interatomic_potential_NN/main/extractor.py
+wget https://raw.githubusercontent.com/J-Fabila/Interatomic_potential_NN/main/extractor.sh
 wget https://raw.githubusercontent.com/J-Fabila/Interatomic_potential_NN/main/input.txt
 
 
 echo  -e \\e[94m "Compiling executables"
 
-chmox +x extractor.py
+chmod +x extractor.sh
 g++ -o forces.exe forces.cpp -lm
 g++ -o geometries.exe distortions.cpp -lm
 
 rm atomicpp.h
 
+echo  -e \\e[32m " #########################################################"
+echo  -e \\e[32m " #################################################### Done"
+echo  -e \\e[32m " #########################################################"
