23d76a95d5a23304a243a1573870d62efaf9805c
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2021-12-21 12:43:32 +0100

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -1,124 +1,191 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set_style("darkgrid")
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#sns.set_style("darkgrid")
 from sklearn.model_selection import train_test_split
 import warnings
+import sys
 warnings.filterwarnings('ignore')
 
-forces  =pd.read_csv("forces.csv",names=["Force"])
-# Centering values
-forces=forces-forces.mean()
-forces_s=forces.shift(-1)
+################################ Lectura de parámetros
+path=sys.argv[1]
+selected_sampling=int(sys.argv[2])
+sampling_size=float(sys.argv[3])
+sampling_parameter=int(sys.argv[4])
+######################################################
+
+############################# Declaración de funciones
+
+def random_sampling(input_data,fraction=0.5):
+    # Works for either forces or energies
+    train, test = train_test_split(input_data, test_size=fraction)
+    accepted_index=list(test.index)
+    pd.DataFrame(accepted_index).to_csv("selected_points.csv", index=False, header=False,sep=' ')
+
+def uniform_sampling_forces(forces,rad=2.0,num_cuantiles=10):
+    # Works exclusively for forces
+    forces=forces-forces.mean()
+    lim_min=forces.Force.mean()-(rad*forces.Force.std() )
+    lim_max=forces.Force.mean()+(rad*forces.Force.std() )
+    cuantiles=np.linspace(lim_max,lim_min,num_cuantiles)
+    frames=[]
+    for i in range(num_cuantiles-1):
+        numerador=forces.loc[(forces['Force']< cuantiles[i]) & (forces['Force']>cuantiles[i+1] )]
+        total_muestras_cuantil=numerador.shape[0]
+        if(i==0):
+            num_muestras=numerador.shape[0]
+        fraccion_necesaria=num_muestras/total_muestras_cuantil
+        if(fraccion_necesaria>1):
+            fraccion_necesaria=1
+        train, test = train_test_split(numerador, test_size=fraccion_necesaria*0.99999)
+        frames.append(test)
+    tot_accepted = pd.concat(frames)
+    numerador=forces.loc[(forces['Force']> (forces.Force.mean()+(rad*forces.Force.std() ) )) | (forces['Force']< (forces.Force.mean()-(rad*forces.Force.std() ) ))]
+    frames.append(numerador)
+    selected=pd.concat(frames)
+    accepted_index=list(selected.index)
+    pd.DataFrame(accepted_index).to_csv("selected_points.csv", index=False, header=False,sep=' ')
+    return len(accepted_index)/forces.shape[0]
+
+def uniform_sampling_energies(energies,rad=2.0,num_cuantiles=10):
+    # Works exclusively for forces
+    energies=energies-energies.mean()
+    lim_min=energies.Uncorrected.mean()-(rad*energies.Uncorrected.std() )
+    lim_max=energies.Uncorrected.mean()+(rad*energies.Uncorrected.std() )
+    cuantiles=np.linspace(lim_max,lim_min,num_cuantiles)
+    frames=[]
+    for i in range(num_cuantiles-1):
+        numerador=energies.loc[(energies['Uncorrected']< cuantiles[i]) & (energies['Uncorrected']>cuantiles[i+1] )]
+        total_muestras_cuantil=numerador.shape[0]
+        if(i==0):
+            num_muestras=numerador.shape[0]
+        fraccion_necesaria=num_muestras/total_muestras_cuantil
+        if(fraccion_necesaria>1):
+            fraccion_necesaria=1
+        train, test = train_test_split(numerador, test_size=fraccion_necesaria*0.99999)
+        frames.append(test)
+    tot_accepted = pd.concat(frames)
+    numerador=energies.loc[(energies['Uncorrected']> (energies.Uncorrected.mean()+(rad*energies.Uncorrected.std() ) )) | (energies['Uncorrected']< (energies.Uncorrected.mean()-(rad*energies.Uncorrected.std() ) ))]
+    frames.append(numerador)
+    selected=pd.concat(frames)
+    accepted_index=list(selected.index)
+    pd.DataFrame(accepted_index).to_csv("selected_points.csv", index=False, header=False,sep=' ')
+    return len(accepted_index)/energies.shape[0]
 
 
-fig, axs = plt.subplots(1, 2, figsize=(20, 10),sharey=True)
-#axs[1].set_title("Histogram",fontsize=20)
-#axs[0].set_title("Average distorsions",fontsize=20)
+def monte_carlo(T,energy,energy_0):
+    k_BT = 0.00008617*T;
+    criterio=np.exp(((energy-energy_0))/k_BT)
+    if criterio > np.random.random(1)[0]:
+        return (criterio,False)
+    else:
+        return (criterio,True)
 
-mean_=[forces["Force"].mean()]*forces.shape[0]
-std_upper=[forces["Force"].mean()+(2*forces["Force"].std())]*forces.shape[0]
-std_lower=[forces["Force"].mean()-(2*forces["Force"].std())]*forces.shape[0]
+v_monte_carlo=np.vectorize(monte_carlo)
 
-fig.suptitle('Forces distribution',fontsize=30)
+def monte_carlo_forces(forces,temp): #solo fuerzas
+    forces=forces-forces.mean()
+    forces_s=forces.shift(-1)
+    forces['Criterio'],forces['Accepted']=v_monte_carlo(temp,forces['Force'],forces_s['Force'])
+    accepted=forces.loc[forces['Accepted']==True]
+    accepted_index=list(accepted.index)
+    pd.DataFrame(accepted_index).to_csv("selected_points.csv", index=False, header=False,sep=' ')
+    return len(accepted.index)/forces.shape[0]
 
-axs[0].set_ylabel('Force [eV/A]',fontsize=20)
-axs[0].set_xlabel('Time step', fontsize=20)
-axs[1].set_xlabel('Frequency', fontsize=20)
-#axs[0].set_xticklabels(fontsize=20)
-axs[0].tick_params(axis="x", labelsize=20)
-axs[0].tick_params(axis="y", labelsize=20)
-axs[1].tick_params(axis="x", labelsize=20)
+def monte_carlo_energies(energies,temp): #solo energias
+    energies=energies-energies.mean()
+    energies_s=energies.shift(-1)
+    energies['Criterio'],energies['Accepted']=v_monte_carlo(temp,energies['Uncorrected'],energies_s['Uncorrected'])
+    accepted=energies.loc[energies['Accepted']==True]
+    accepted_index=list(accepted.index)
+    pd.DataFrame(accepted_index).to_csv("selected_points.csv", index=False, header=False,sep=' ')
+    return len(accepted.index)/energies.shape[0]
 
+def cutted_trajectory(input_data,frac=0.5):
+    # Funciona para fuerzas y energas
+    long=input_data.shape[0]
+    limit=int(long*fraction)
+    input_data=input_data[:limit]
+    accepted_index=list(input_data.index)
+    pd.DataFrame(accepted_index).to_csv("cutted_energy_points.csv", index=False, header=False,sep=' ')
 
-axs[1].hist(forces["Force"],orientation="horizontal",bins=30)
-axs[0].plot(forces["Force"],label="Accepted  points",linestyle='none',marker='o',markersize=3)
+######################################################
 
-#plt.xticks(fontsize=15) # Asigna tamaño de fuente = 15 a los números en el eje x
-#plt.yticks(fontsize=15) # Asigna tamaño de fuente = 15 a los números en el eje y
-axs[0].plot(mean_,label="Mean",linewidth=4.0,marker='o')
-axs[0].plot(std_lower,label="Mean+2*Std",linewidth=4.0,marker='o')
-axs[0].plot(std_upper,label="Mean-2*Std",linewidth=4.0,marker='o')
-axs[0].legend(loc="upper left",fontsize=15)
+##################################### Lectura de datos
+if sampling_parameter == 0: # Fuerzas
+    path=path+"/forces.csv"
+    forces  =pd.read_csv(path,names=["Force"])
+elif sampling_parameter == 1: #Energias
+    path=path+"/energies.csv"
+    energies  =pd.read_csv(path,names=["Corrected","Uncorrected","Free"])
+######################################################
 
-#axs[1].plot(mean_,label="Mean",linewidth=4.0,marker='o')
-#axs[1].plot(std_lower,label="Mean+Std",linewidth=4.0,marker='o')
-#axs[1].plot(std_upper,label="Mean-Std",linewidth=4.0,marker='o')
+if selected_sampling == 1:
+   #Monte Carlo
+    if sampling_parameter == 0: #Forces
+        fraction=sampling_size
+        i=50
+        temp=1
+        frac=1
+        tolerancia=0.025
+        while  ( ((frac+tolerancia)<fraction) | ((frac-tolerancia)>fraction)):
+            frac=monte_carlo_forces(forces,temp)
+        #    print(frac,temp)
+            temp=temp+i
+            if temp>5000:
+                break
+    elif sampling_parameter ==1: #Energies
+        fraction=sampling_size
+        i=50
+        temp=1
+        frac=1
+        tolerancia=0.025
+        while  ( ((frac+tolerancia)<fraction) | ((frac-tolerancia)>fraction)):
+            frac=monte_carlo_energies(energies,temp)
+        #    print(frac,temp)
+            temp=temp+i
+            if temp>5000:
+                break
 
-fig.savefig("forces_hist.png") # Esto guarda la figura como en formato png.
+elif selected_sampling == 2:
+   #Uniform distribution
+    if sampling_parameter == 0: #Forces
+        fraction=sampling_size
+        frac=1.0
+        i=0.05
+        rad=1
+        tolerancia=0.025
+        while  ( ((frac+tolerancia)<fraction) | ((frac-tolerancia)>fraction)):
+            frac=uniform_sampling_forces(forces,rad,10)
+#            print(frac,rad)
+            rad=rad+i
+            if rad>5:
+                break
 
-forces  =pd.read_csv("forces.csv",names=["Force"])
+    elif sampling_parameter ==1: #Energies
+        fraction=sampling_size
+        frac=1
+        i=0.05
+        rad=1
+        tolerancia=0.025
+        while  ( ((frac+tolerancia)<fraction) | ((frac-tolerancia)>fraction)):
+            frac=uniform_sampling_energies(energies,rad,10)
+#            print(frac,rad)
+            rad=rad+i
+            if rad>5:
+                break
 
-###################### Parameters
-rad=2.5               # times STD
-num_cuantiles=10 # N of quantiles
-#################################
+elif selected_sampling == 3:
+   #Random
+   if sampling_parameter == 0: #Forces
+       random_sampling(forces,sampling_size)
+   elif sampling_parameter ==1: #Energies
+       random_sampling(energies,sampling_size)
+elif selected_sampling == 4:
+   #Cutted trajectory
+   if sampling_parameter == 0: #Forces
+       cutted_trajectory(forces,sampling_size)
+   elif sampling_parameter ==1: #Energies
+       cutted_trajectory(energies,sampling_size)
 
-forces=forces-forces.mean()
-lim_min=forces.Force.mean()-(rad*forces.Force.std() )
-lim_max=forces.Force.mean()+(rad*forces.Force.std() )
-cuantiles=np.linspace(lim_max,lim_min,num_cuantiles)
-frames=[]
-for i in range(num_cuantiles-1):
-    #print(cuantiles[i],cuantiles[i+1])
-    numerador=forces.loc[(forces['Force']< cuantiles[i]) & (forces['Force']>cuantiles[i+1] )]
-    total_muestras_cuantil=numerador.shape[0]
-    #print(total_muestras_cuantil)
-    if(i==0):
-        num_muestras=numerador.shape[0]
-     #   print(num_muestras)
-    fraccion_necesaria=num_muestras/total_muestras_cuantil
-    if(fraccion_necesaria>1):
-        fraccion_necesaria=1
-    #print(fraccion_necesaria)
-    #sklearn fraction fraccion necesaria
-    train, test = train_test_split(numerador, test_size=fraccion_necesaria*0.99999)
-    #print("test",test.shape)
-    frames.append(test)
-
-tot_accepted = pd.concat(frames)
-numerador=forces.loc[(forces['Force']> (forces.Force.mean()+(2*forces.Force.std() ) )) | (forces['Force']< (forces.Force.mean()-(2*forces.Force.std() ) ))]
-frames.append(numerador)
-selected=pd.concat(frames)
-
-#fig, axs = plt.subplots(1, 2, figsize=(20, 10),sharey=True)
-#axs[1].hist(tot_accepted["Force"],orientation="horizontal",bins=50)
-#axs[0].plot(tot_accepted["Force"],label="Accepted  points",linestyle='none',marker='o',markersize=3)
-
-print("Selected points: ",selected.shape[0]," out of ",forces.shape[0], "(",selected.shape[0]/forces.shape[0]*100,"%)")
-
-
-numerador=forces.loc[(forces['Force']> (forces.Force.mean()+(rad*forces.Force.std() ) )) | (forces['Force']< (forces.Force.mean()-(rad*forces.Force.std() ) ))]
-
-fig, axs = plt.subplots(1, 2, figsize=(20, 10),sharey=True)
-
-fig.suptitle('Uniform sampling from normal distribution (Forces)',fontsize=30)
-
-axs[0].set_ylabel('Force [eV/A]',fontsize=20)
-axs[0].set_xlabel('Time step', fontsize=20)
-axs[1].set_xlabel('Frequency', fontsize=20)
-#axs[0].set_xticklabels(fontsize=20)
-axs[0].tick_params(axis="x", labelsize=20)
-axs[0].tick_params(axis="y", labelsize=20)
-axs[1].tick_params(axis="x", labelsize=20)
-
-axs[1].hist(forces["Force"],orientation="horizontal",bins=30,color="orange")
-axs[0].plot(forces["Force"],label="Rejected  points",linestyle='none',marker='o',markersize=3,color="orange")
-
-axs[1].hist(numerador["Force"],orientation="horizontal",bins=30,color="blue")
-axs[0].plot(numerador["Force"],label="Accepted  points",linestyle='none',marker='o',markersize=3,color="blue")
-
-axs[1].hist(tot_accepted["Force"],orientation="horizontal",bins=10,color="blue")
-axs[0].plot(tot_accepted["Force"],linestyle='none',marker='o',markersize=3,color="blue")
-axs[0].legend(loc="lower right",fontsize=15)
-
-#axs[1].plot(mean_,label="Mean",linewidth=4.0,marker='o')
-#axs[1].plot(std_lower,label="Mean+Std",linewidth=4.0,marker='o')
-#axs[1].plot(std_upper,label="Mean-Std",linewidth=4.0,marker='o')
-
-fig.savefig("selected_forces.png") # Esto guarda la figura como en formato png.
-
-accepted_index=list(tot_accepted.index)
-
-pd.DataFrame(tot_accepted.index).to_csv("selected_points.csv", index=False, header=False,sep=' ')
